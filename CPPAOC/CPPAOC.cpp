260663af78d22e3d1cf192fef526348cbba858df
Rogier Hans <rogierhans@gmail.com>
2022-01-04 00:59:20 +0100

    Day20

--- CPPAOC/CPPAOC.cpp
+++ CPPAOC/CPPAOC.cpp
@@ -14,9 +14,15 @@ using namespace std;
 
 int main()
 {
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	
 	//day01_part1();
 	//day01_part2();
 	day20_part1();
 	//day01_part2();
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
+
 	return 0;
 }
--- CPPAOC/Day20.cpp
+++ CPPAOC/Day20.cpp
@@ -11,37 +11,63 @@
 
 using namespace std;
 
+void print(const std::vector<std::vector<int>>& v) {
+	for (size_t i = 1; i < v.size(); i++)
+	{
+		for (size_t j = 0; j < v[i].size(); j++)
+		{
+			cout << v[i][j];
+		}
+		cout << "\n";
+	}
+}
+
 int day20_part1()
 {
 	fstream newfile;
 	vector<string> list;
 	newfile.open("C:\\Users\\Rogier\\Dropbox\\AOC2\\AOC2\\InputFiles\\2021_20\\input.txt", ios::in);
+	int map[512];
 	if (newfile.is_open()) {
+		string first;
+		getline(newfile, first);
+		for (size_t i = 0; i < first.length(); i++)
+		{
+			map[i] = (first[i] == '#' ? 1 : 0);	
+		}
 		string tp;
 		while (getline(newfile, tp)) {
-		 list.push_back(tp);
+			list.push_back(tp);
 		}
 	}
 	int counterPart1 = 0;
-	int array[250][250]{};
+	const int height = 250;
+	int array[height][height]{};
 	for (size_t i = 1; i < list.size(); i++)
 	{
 		for (size_t j = 0; j < list[i].length(); j++)
 		{
-			array[i][j] = list[i][j] == '#' ? 1 : 0;
+			array[i + 75][j + 75] = (list[i][j] == '#' ? 1 : 0);
 		}
 	}
-
-
-	for (size_t i = 1; i < 250; i++)
+	/*
+	for (size_t i = 0; i < 75; i++)
 	{
-		for (size_t j = 0; j < 250; j++)
+		for (size_t j = 0; j < 75; j++)
 		{
-			cout << array[i][j] ;
+			cout << array[i + 75][j + 75];
 		}
 		cout << "\n";
+	}*/
+	int count = 0;
+	for (size_t i = 0; i < height; i++)
+	{
+		for (size_t j = 0; j < height; j++)
+		{
+			if (array[i][j] == 1) count++;
+		}
 	}
-
+	cout << count << endl;
 
 	return 0;
 }
